721adff1f775326c9721f9a1ec5a023727db4625
alexeev-sergey1978 <128303569+alexeev-sergey1978@users.noreply.github.com>
2023-03-20 00:31:54 +1000

    init

---
+++ 2index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click' , function() {
+    output.innerHTML = '🥰С днем рождения,милая😘';
+});
